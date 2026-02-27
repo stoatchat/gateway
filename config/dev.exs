db5f5ba98c8616a2c22a51b62deb5d54ef090f01
RR <twitch7443@protonmail.com>
2026-02-27 01:53:43 +0000

    feat: build foundations for configurations

---
+++ config/dev.exs
@@ -0,0 +1,5 @@
+import Config
+
+config :stoat_gateway,
+  ws_port: 4000
+  
--- lib/stoat_gateway/application.ex
+++ lib/stoat_gateway/application.ex
@@ -1,6 +1,4 @@
 defmodule StoatGateway.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
   @moduledoc false
 
   use Application
@@ -8,12 +6,10 @@ defmodule StoatGateway.Application do
   @impl true
   def start(_type, _args) do
     children = [
-      {Bandit, plug: StoatGateway.Web.Router, scheme: :http},
+      {Bandit, plug: StoatGateway.Web.Router, scheme: :http, port: Application.get_env(:stoat_gateway, :ws_port)},
       StoatGateway.Events.Consumer
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: StoatGateway.Supervisor]
     Supervisor.start_link(children, opts)
   end
