fc64a93d210957703aa1279073d025a4cf103ae8
RR <twitch7443@protonmail.com>
2026-03-03 01:13:13 +0000

    feat: basic query param checking

--- lib/stoat_gateway/web/socket_handler.ex
+++ lib/stoat_gateway/web/socket_handler.ex
@@ -1,8 +1,17 @@
 defmodule StoatGateway.Web.SocketHandler do
   require Logger
 
-  def init(state) do
-    {:ok, state}
+  defstruct [ready: false, format: :etf]
+  @type t :: %__MODULE__{ready: boolean, format: String.t()}
+
+
+  def init(query_params) do
+    format = case Map.fetch(query_params, "format") do
+      {:ok, "etf"} -> :etf
+      {:ok, _} -> :json
+      _ -> :json 
+    end
+    {:ok, %__MODULE__{ready: true, format: format}}
   end
   
   def handle_in({text, opcode: :text}, state) do
