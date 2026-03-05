368b45168e1848b2df3dd1e9ce2ad60171976423
RR <twitch7443@protonmail.com>
2026-03-05 17:13:06 +0000

    feat: Add data handling for WS

--- lib/stoat_gateway/web/socket_handler.ex
+++ lib/stoat_gateway/web/socket_handler.ex
@@ -14,8 +14,25 @@ defmodule StoatGateway.Web.SocketHandler do
     {:ok, %__MODULE__{ready: true, format: format}}
   end
   
-  def handle_in({text, opcode: :text}, state) do
-    {:push, {:text, text}, state}
+  def handle_in({frame, opcode: _}, state) do
+    data = decode_frame(frame, state.format)
+    case data do
+      {:ok, payload} -> handle_payload(payload, state)
+      _ -> {:stop, :normal, 1007, encode_frame(%{error: "invalid"}, state.format), state} # TODO: Correct error format
+    end
+  end
+  
+  # Not sure about pattern matching the whole thing yet-
+  # Might be a bit ugly as we go- 
+  # v2 proto should definitely have an Enum for type and a consistent data key
+  # then we can just case a payload and extract those and handle each event as
+  # def handle_payload(EVENT_TYPE, %Stoat.TypingEvent{} = data, state) do...
+  def handle_payload(%{"type" => "StartTyping"} = payload, state) do
+    {:push, encode_frame(%{test: "test"}, state.format), state}
+  end
+
+  def handle_payload(_, state) do
+    {:stop, :normal, "bruh?",state}
   end
 
   def handle_info(_, state) do
@@ -29,4 +46,25 @@ defmodule StoatGateway.Web.SocketHandler do
   def terminate(:remote, state) do
     {:ok, state}
   end 
+
+  def terminate(:normal, state) do
+    # Some sort of clean-up here
+    {:ok, state}
+  end
+
+  defp encode_frame(frame, :json) do
+    {:text, Jason.encode!(frame)}
+  end
+
+  defp encode_frame(frame, :etf) do
+    {:binary, :erlang.term_to_binary(frame)}
+  end
+
+  defp decode_frame(frame, :json) do
+    Jason.decode(frame)
+  end
+
+  defp decode_frame(frame, :etf) do
+      :erlang.binary_to_term(frame)
+  end
 end
