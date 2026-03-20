2e68fa321fd6cadfca8fba7e7f377e528b994298
RR <twitch7443@protonmail.com>
2026-03-20 19:56:57 +0000

    feat: Add mongo driver to supervisor
    Signed-off-by: RR <twitch7443@protonmail.com>

--- lib/stoat_gateway.ex
+++ lib/stoat_gateway.ex
@@ -5,6 +5,7 @@ defmodule StoatGateway do
   def start(_type, _args) do
     children = [
       {Bandit, plug: StoatGateway.Web.Router, scheme: :http, port: Application.get_env(:stoat_gateway, :ws_port)},
+      {Mongo, [name: :mongo_db, url: Application.get_env(:stoat_gateway, :mongodb), pool_size: 2]},
       StoatGateway.Events.Consumer
     ]
 
