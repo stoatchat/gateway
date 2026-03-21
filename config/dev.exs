a6db5af53895f0fafa017425da040ed380c94241
RR <twitch7443@protonmail.com>
2026-03-21 00:42:39 +0000

    fix(config): update database value
    Signed-off-by: RR <twitch7443@protonmail.com>

--- config/dev.exs
+++ config/dev.exs
@@ -2,5 +2,5 @@ import Config
 
 config :stoat_gateway,
   ws_port: 4000,
-  mongodb: "mongodb://localhost:27017/db",
+  mongodb: "mongodb://localhost:27017/revolt",
   rabbit: "amqp://rabbituser:rabbitpass@localhost"
