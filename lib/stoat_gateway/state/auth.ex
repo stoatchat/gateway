5b8b63268b7fdface397b8a5b0fcffa9cf2078a7
RR <twitch7443@protonmail.com>
2026-03-21 00:42:10 +0000

    feat: add token fetch methods
    Signed-off-by: RR <twitch7443@protonmail.com>

---
+++ lib/stoat_gateway/state/auth.ex
@@ -0,0 +1,20 @@
+defmodule StoatGateway.Auth do
+
+  def find_by_token(token) when not is_nil(token) do 
+    # Potentially rethink?
+    case user_from_token(token) do
+      %{"user_id" => _user_id} = payload -> {:user, payload}
+      _ -> case bot_from_token(token) do
+        %{"_id" => _bot_id} = payload -> {:bot, payload}
+        _ -> nil
+      end
+    end
+  end
+  
+  # Avoid calling db if nil
+  def find_by_token(nil), do: nil
+
+
+  defp user_from_token(token), do: Mongo.find_one(:mongo_db, "sessions", %{"token" => token})
+  defp bot_from_token(token), do: Mongo.find_one(:mongo_db, "bots", %{"token" => token})
+end
