12592f0c950cb49e92da207e9e8e79a8ac7db293
RR <twitch7443@protonmail.com>
2026-03-24 22:20:08 +0000

    feat: send ready payload and update state
    Signed-off-by: RR <twitch7443@protonmail.com>

--- lib/stoat_gateway/state/session.ex
+++ lib/stoat_gateway/state/session.ex
@@ -17,7 +17,8 @@ defmodule StoatGateway.Session do
   defstruct [ready: false, user_id: nil, session: nil, linked_socket: nil, type: :user]
   @type t :: %__MODULE__{ready: boolean, user_id: String.t(), session: String.t(), linked_socket: pid(), type: atom()}
 
-  def start_link(%{socket: socket, data: %{"user_id" => id, "_id" => session}}) do
+  def start_link(%{socket: socket, data: %{"user_id" => id, "_id" => session} = data}) do
+    IO.inspect(data)
     GenServer.start_link(__MODULE__, %__MODULE__{user_id: id, linked_socket: socket, session: session})
   end
 
@@ -28,9 +29,9 @@ defmodule StoatGateway.Session do
   end
 
   def handle_continue(:ready, state) do
-    # TODO: Fill with data from Mongo
+
     ready_payload = %Stoat.State.Ready{}
     send(state.linked_socket, {:ready, ready_payload})
-    {:noreply, state}
+    {:noreply, %{state | ready: true}}
   end
 end
