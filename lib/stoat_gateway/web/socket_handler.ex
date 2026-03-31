e0ac160fa6cb71d424fcc05ba6e4bd9e0822d1bc
ShaksterNano <54268387+shaksternano@users.noreply.github.com>
2026-03-31 23:45:37 +0100

    style: make WebSock behaviour explicit
    Signed-off-by: RR <twitch7443@protonmail.com>

--- lib/stoat_gateway/web/socket_handler.ex
+++ lib/stoat_gateway/web/socket_handler.ex
@@ -2,22 +2,24 @@ defmodule StoatGateway.Web.SocketHandler do
   require Logger
   alias StoatGateway.Session
 
+  @behaviour WebSock
+
   defstruct [ready: false, format: :json]
   @type t :: %__MODULE__{ready: boolean, format: String.t()}
 
-
+  @impl true
   def init(query_params) do
     format = case Map.fetch(query_params, "format") do
       {:ok, "etf"} -> :etf
       {:ok, "msgpack"} -> :msgpack
       _ -> :json 
     end
-    
+
     user = case Map.fetch(query_params, "token") do
       {:ok, token} -> StoatGateway.Auth.find_by_token(token)
       _ -> nil
     end
-    
+
     # TODO(twitch): Honestly just make the above error and we can just catch that in terminate
     case user do
       {type, data} -> 
@@ -27,7 +29,8 @@ defmodule StoatGateway.Web.SocketHandler do
     end
     {:ok, %__MODULE__{ready: true, format: format}}
   end
-  
+
+  @impl true
   def handle_in({frame, opcode: _}, state) do
     data = decode_frame(frame, state.format)
     case data do
@@ -35,13 +38,12 @@ defmodule StoatGateway.Web.SocketHandler do
       _ -> {:stop, :normal, 1007, encode_frame(%{error: "invalid"}, state.format), state} # TODO: Correct error format
     end
   end
-  
+
   # Not sure about pattern matching the whole thing yet-
   # Might be a bit ugly as we go- 
   # v2 proto should definitely have an Enum for type and a consistent data key
   # then we can just case a payload and extract those and handle each event as
   # def handle_payload(EVENT_TYPE, %Stoat.TypingEvent{} = data, state) do...
-
 
   def handle_payload(%{"type" => "Ping"} = _payload, state) do
     {:push, encode_frame(%{type: "Pong", data: System.os_time()}, state.format), state}
@@ -50,32 +52,36 @@ defmodule StoatGateway.Web.SocketHandler do
   def handle_payload(%{"type" => "BeginTyping"} = _payload, state) do
     {:push, encode_frame(%{test: "test"}, state.format), state}
   end
-  
+
   def handle_payload(%{"type" => "EndTyping"} = _payload, state) do
     {:push, encode_frame(%{test: "test"}, state.format), state}
   end
-
 
   def handle_payload(_, state) do
     {:stop, :normal, 1007, encode_frame(%{"error" => "invalid payload"}, state.format),state}
   end
 
+  @impl true
   def handle_info({:ready, payload}, state) do
     {:push, encode_frame(payload, state.format), state}
   end
 
+  @impl true
   def handle_info(_, state) do
     {:ok, state}
   end
 
+  @impl true
   def terminate(:timeout, state) do
     {:ok, state}  
   end
 
+  @impl true
   def terminate(:remote, state) do
     {:ok, state}
-  end 
+  end
 
+  @impl true
   def terminate(:normal, state) do
     # Some sort of clean-up here
     {:ok, state}
