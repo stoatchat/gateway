d7db7f414d1fbdd2048110e4469fbfe05275cbef
RR <twitch7443@protonmail.com>
2026-05-19 22:22:47 +0100

    fix: ignore runtime config if not prod build

--- config/runtime.exs
+++ config/runtime.exs
@@ -1,6 +1,8 @@
 import Config
 
-config :stoat_gateway,
-  ws_port: System.get_env("WS_PORT"),
-  mongodb: System.get_env("MONGODB_URI"),
-  rabbit: System.get_env("RABBITMQ_URI")
+if config_env() == :prod do
+  config :stoat_gateway,
+    ws_port: System.get_env("WS_PORT"),
+    mongodb: System.get_env("MONGODB_URI"),
+    rabbit: System.get_env("RABBITMQ_URI")
+end
