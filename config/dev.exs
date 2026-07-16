38520cc38445181f5ca5896603a468c78ba3df1b
RR <twitch7443@protonmail.com>
2026-07-16 22:44:59 +0100

    chore: use config file in prod release
    Signed-off-by: RR <twitch7443@protonmail.com>

--- config/dev.exs
+++ config/dev.exs
@@ -1,7 +1,7 @@
 import Config
 
 config :stoat_gateway,
-  ws_port: 4000,
+  ws_port: System.get_env("WS_PORT", "4000"),
   mongodb: "mongodb://localhost:27017/",
   rabbit: [
     host: "localhost",
--- config/runtime.exs
+++ config/runtime.exs
@@ -1,9 +1,28 @@
 import Config
 
 if config_env() == :prod do
+  revolt =
+    case Toml.decode_file("Revolt.toml") do
+      {:ok, config} -> config
+      {:error, _} -> %{}
+    end
+
   config :stoat_gateway,
-    ws_port: System.get_env("WS_PORT", "4000"),
-    mongodb: System.get_env("MONGODB_URI", "mongodb://localhost:27017/revolt"),
-    rabbit: System.get_env("RABBITMQ_URI", "amqp://rabbituser:rabbitpass@localhost"),
-    redis: System.get_env("REDIS_URI", "redis://localhost:6379")
+    ws_port: System.get_env("WS_PORT", "14703"),
+    mongodb:
+      get_in(revolt, ["database", "mongodb"]) ||
+        System.get_env("MONGODB_URI", "mongodb://localhost:27017/"),
+    rabbit: [
+      host: get_in(revolt, ["rabbit", "host"]) || System.get_env("RABBIT_HOST", "localhost"),
+      port: get_in(revolt, ["rabbit", "port"]) || System.get_env("RABBIT_PORT", "5672"),
+      username:
+        get_in(revolt, ["rabbit", "username"]) ||
+          System.get_env("RABBIT_USERNAME", "rabbituser"),
+      password:
+        get_in(revolt, ["rabbit", "password"]) ||
+          System.get_env("RABBIT_PASSWORD", "rabbitpass")
+    ],
+    redis:
+      get_in(revolt, ["database", "redis"]) ||
+        System.get_env("REDIS_URI", "redis://localhost:6379")
 end
