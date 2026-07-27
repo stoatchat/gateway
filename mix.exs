a361c52973c6e4c593c273011d553d9b59ad69df
RR <twitch@twitch.dance>
2026-07-28 00:19:39 +0100

    feat: add peep telemetry from events
    Signed-off-by: RR <twitch@twitch.dance>

--- mix.exs
+++ mix.exs
@@ -32,6 +32,7 @@ defmodule StoatGateway.MixProject do
       {:toml, "~> 0.7"},
       {:telemetry, "~> 1.4"},
       {:telemetry_metrics, "~> 1.1"},
+      {:telemetry_poller, "~> 0.4"},
       {:peep, "~> 5.0"},
       {:ecto_ulid_next, "~> 1.0"},
       {:needle_ulid, git: "https://github.com/bonfire-networks/needle_ulid", ref: "c48e90b"}
