803f7f30de775cb864435627d8296149445b64d3
RR <twitch@twitch.dance>
2026-07-30 23:23:19 +0100

    fix: correctly route call update
    Signed-off-by: RR <twitch@twitch.dance>

--- lib/stoat_gateway/events/consumer.ex
+++ lib/stoat_gateway/events/consumer.ex
@@ -102,7 +102,7 @@ defmodule StoatGateway.Events.Consumer do
   def handle_event("VoiceChannelJoin", data), do: handle_channel_event(:VoiceChannelJoin, data)
   def handle_event("VoiceChannelLeave", data), do: handle_channel_event(:VoiceChannelLeave, data)
   def handle_event("VoiceChannelMove", data), do: handle_channel_event(:VoiceChannelMove, data)
-  def handle_event("VoiceCallUpdate", data), do: handle_channel_event(:VoiceCallUpdate, data)
+  def handle_event("VoiceCallUpdate", data), do: handle_presence_event(:VoiceCallUpdate, data)
 
   def handle_event("WebhookCreate", data), do: handle_channel_event(:WebhookCreate, data)
   def handle_event("WebhookUpdate", data), do: handle_channel_event(:WebhookUpdate, data)
